00beba3f1d9ba1d05b1ddfc760fadfc913868da2
jthicks91 <jordanthicks91@gmail.com>
2019-01-26 11:23:47 -0800

    Finished MVP for JS Sprint. Attempting stretch now.

--- challenges/classes.js
+++ challenges/classes.js
@@ -27,7 +27,7 @@ const cubeObject = new CuboidMaker({
 });
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cubeObject.volume()); // 100
-console.log(cubeObject.surfaceArea()); // 130
+// console.log(cubeObject.volume()); // 100
+// console.log(cubeObject.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
